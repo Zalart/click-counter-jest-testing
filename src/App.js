957641a32274ceeab8195fb04a3c8f356892088e
Zalart <me@zalart.com>
2021-12-26 20:42:47 +0300

    Counters implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(0);
+    const [error, setError] = useState(false);
+    const incrementHandler = () => {
+        error && setError(false);
+        setCount(count+1);
+    }
+    const decrementHandler = () => {
+        if (count===0) {
+            setError(true)
+        } else {
+            setCount(count - 1)
+         }
+    }
   return (
-    <div data-test="component-app">
-     <h1>App</h1>
+    <div className="App" data-test="component-app">
+     <h1 data-test="counter-display">The counter: <span data-test="count">{count}</span></h1>
+        {error && <div className="error-message">The counter can't be lower than "0"</div>}
+        <button data-test="increment-button" onClick={incrementHandler}>Increment counter</button>
+        <button data-test="decrement-button" onClick={decrementHandler}>Decrement counter</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,24 +4,54 @@ import App from './App';
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
 
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setUp = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, value) => wrapper.find(`[data-test='${value}']`);
+
 test('renders without error', ()=> {
-const wrapper = shallow(<App />);
-const appComponent = wrapper.find("[data-test='component-app']");
+const wrapper = setUp();
+// eslint-disable-next-line testing-library/await-async-query
+const appComponent = findByTestAttr(wrapper, 'component-app');
 expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button', ()=> {
-
+    const wrapper = setUp();
+    // eslint-disable-next-line testing-library/await-async-query
+    const button = findByTestAttr(wrapper, 'increment-button')
+    expect(button.length).toBe(1);
 })
 
 test('renders counter display', () => {
-
+    const wrapper = setUp();
+    // eslint-disable-next-line testing-library/await-async-query
+    const counter = findByTestAttr(wrapper, 'counter-display');
+    expect(counter.length).toBe(1);
 })
 
 test('counter display starts at 0', ()=> {
+    const wrapper = setUp();
+    // eslint-disable-next-line testing-library/await-async-query
+    const counterSpanText = findByTestAttr(wrapper, 'count').text();
 
+    expect(counterSpanText).toBe('0');
 })
 
 test('clicking the button increments the counter display', ()=> {
+    const wrapper = setUp();
+    // eslint-disable-next-line testing-library/await-async-query
+    const counter = findByTestAttr(wrapper, 'count').text();
+    // eslint-disable-next-line testing-library/await-async-query
+    const buttonClick = findByTestAttr(wrapper, 'increment-button').simulate('click');
+    // eslint-disable-next-line testing-library/await-async-query
+    const incrementedCounter = findByTestAttr(wrapper, 'count').text();
+    expect(+counter + 1).toBe(+incrementedCounter);
+
+
 
 })
