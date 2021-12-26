5765a3ba511f9245cb50264783f0e7150cb9feec
Zalart <me@zalart.com>
2021-12-27 01:56:41 +0300

    Enzyme tests for increment counter and error message block

--- src/App.js
+++ src/App.js
@@ -10,16 +10,12 @@ function App() {
         setCount(count+1);
     }
     const decrementHandler = () => {
-        if (count===0) {
-            setError(true)
-        } else {
-            setCount(count - 1)
-         }
+        count>0 ? setCount(count-1) : setError(true)
     }
   return (
     <div className="App" data-test="component-app">
      <h1 data-test="counter-display">The counter: <span data-test="count">{count}</span></h1>
-        {error && <div className="error-message">The counter can't be lower than "0"</div>}
+        {error && <div className="error-message" data-test="error-message">The counter can't be lower than 0</div>}
         <button data-test="increment-button" onClick={incrementHandler}>Increment counter</button>
         <button data-test="decrement-button" onClick={decrementHandler}>Decrement counter</button>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -13,45 +13,103 @@ const setUp = () => shallow(<App />);
 
 const findByTestAttr = (wrapper, value) => wrapper.find(`[data-test='${value}']`);
 
-test('renders without error', ()=> {
-const wrapper = setUp();
+describe('Elements rendered', () => {
+    test('renders without error', ()=> {
+        const wrapper = setUp();
 // eslint-disable-next-line testing-library/await-async-query
-const appComponent = findByTestAttr(wrapper, 'component-app');
-expect(appComponent.length).toBe(1);
+        const appComponent = findByTestAttr(wrapper, 'component-app');
+        expect(appComponent.length).toBe(1);
+    });
+
+    test('renders counter display', () => {
+        const wrapper = setUp();
+        // eslint-disable-next-line testing-library/await-async-query
+        const counter = findByTestAttr(wrapper, 'counter-display');
+        expect(counter.length).toBe(1);
+    })
+
+    test('counter display starts at 0', ()=> {
+        const wrapper = setUp();
+        // eslint-disable-next-line testing-library/await-async-query
+        const counterSpanText = findByTestAttr(wrapper, 'count').text();
+        expect(counterSpanText).toBe('0');
+    })
 });
 
-test('renders increment button', ()=> {
-    const wrapper = setUp();
-    // eslint-disable-next-line testing-library/await-async-query
-    const button = findByTestAttr(wrapper, 'increment-button')
-    expect(button.length).toBe(1);
-})
+describe('Increment action', function () {
+    test('renders increment button', ()=> {
+        const wrapper = setUp();
+        // eslint-disable-next-line testing-library/await-async-query
+        const button = findByTestAttr(wrapper, 'increment-button')
+        expect(button.length).toBe(1);
+    })
 
-test('renders counter display', () => {
-    const wrapper = setUp();
-    // eslint-disable-next-line testing-library/await-async-query
-    const counter = findByTestAttr(wrapper, 'counter-display');
-    expect(counter.length).toBe(1);
-})
+    test('clicking the button increments the counter display', ()=> {
+        const wrapper = setUp();
+        // eslint-disable-next-line testing-library/await-async-query
+        const counter = findByTestAttr(wrapper, 'count').text();
+        // eslint-disable-next-line testing-library/await-async-query
+        const buttonClick = findByTestAttr(wrapper, 'increment-button').simulate('click');
+        // eslint-disable-next-line testing-library/await-async-query
+        const incrementedCounter = findByTestAttr(wrapper, 'count').text();
+        expect(+counter + 1).toBe(+incrementedCounter);
+    })
+});
 
-test('counter display starts at 0', ()=> {
-    const wrapper = setUp();
-    // eslint-disable-next-line testing-library/await-async-query
-    const counterSpanText = findByTestAttr(wrapper, 'count').text();
+describe('Decrement action', function () {
+    test('renders decrement button', ()=> {
+        const wrapper = setUp();
+        // eslint-disable-next-line testing-library/await-async-query
+        const decrementButton = findByTestAttr(wrapper, 'decrement-button');
+        expect(decrementButton.length).toBe(1);
+    })
 
-    expect(counterSpanText).toBe('0');
-})
+    test('clicking the decrement button decrements the counter', () => {
+        const wrapper = setUp();
+        // eslint-disable-next-line testing-library/await-async-query
+        const incrementClick = findByTestAttr(wrapper, 'increment-button').simulate('click');
+        // eslint-disable-next-line testing-library/await-async-query
+        const decrementClick = findByTestAttr(wrapper, 'decrement-button').simulate('click');
+        // eslint-disable-next-line testing-library/await-async-query
+        const counter = findByTestAttr(wrapper, 'count').text();
+        expect(counter).toBe("0");
+    })
+});
+    describe('Error block', ()=> {
+        test('Error block is rendered only when needed', () => {
+            const wrapper = setUp();
+            // eslint-disable-next-line testing-library/await-async-query
+            const errorBlock = findByTestAttr(wrapper, 'error-block');
+            expect(errorBlock.length).toBe(0);
+        })
+        describe('Decrement click and error block interaction', function () {
+            let wrapper;
+            beforeEach(()=> {
+                wrapper = setUp();
+                // eslint-disable-next-line testing-library/await-async-query
+                const decrementClick = findByTestAttr(wrapper, 'decrement-button').simulate('click');
+            })
 
-test('clicking the button increments the counter display', ()=> {
-    const wrapper = setUp();
-    // eslint-disable-next-line testing-library/await-async-query
-    const counter = findByTestAttr(wrapper, 'count').text();
-    // eslint-disable-next-line testing-library/await-async-query
-    const buttonClick = findByTestAttr(wrapper, 'increment-button').simulate('click');
-    // eslint-disable-next-line testing-library/await-async-query
-    const incrementedCounter = findByTestAttr(wrapper, 'count').text();
-    expect(+counter + 1).toBe(+incrementedCounter);
+            test('Display Error block when decrement button is clicked when counter is 0', ()=> {
+                // eslint-disable-next-line testing-library/await-async-query
+                const errorMessage = findByTestAttr(wrapper, 'error-message');
+                expect(errorMessage.length).toBe(1);
+            })
+
+            test('Stay on 0 when decrement click on 0 counter state', ()=> {
+                // eslint-disable-next-line testing-library/await-async-query
+                const counter = findByTestAttr(wrapper, 'count').text();
+                expect(counter).toBe("0");
+            })
+            test('Remove error message when increment button click', ()=> {
+                // eslint-disable-next-line testing-library/await-async-query
+                const incrementClick = findByTestAttr(wrapper, 'increment-button').simulate('click');
+                // eslint-disable-next-line testing-library/await-async-query
+                const errorMessage = findByTestAttr(wrapper, 'errorMessage');
+                expect(errorMessage.length).toBe(0);
+            })
+        });
+
+    })
 
 
-
-})
